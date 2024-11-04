8c75b52d5e55976fb31e375b30114557f0dfd932
kdowd <kevin.dowd@gmail.com>
2024-11-04 14:52:32 +1300

    starter

--- libcurl.cpp
+++ libcurl.cpp
@@ -1,9 +1,13 @@
-
 #include <iostream>
-
+#include <string>
 #include "curl/curl.h"
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	CURL* curl{};
+	CURLcode res;
+
+	std::string url = "https://example.com/foo.asp?name=dddd";
+	//curl_easy_setopt(curl, CURLOPT_URL, "https://example.com/file.txt");
+
 }
